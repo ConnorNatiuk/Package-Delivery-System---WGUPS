26e7406cd04a4c80ab719cafb6a4948dd8d0b92c
cnatiu2 <cnatiu2@wgu.edu>
2026-03-24 09:50:21 -0700

    Added documentation to Location.py

--- Location.py
+++ Location.py
@@ -1,8 +1,33 @@
 class Location:
+
+    """
+    DESC: Represents a location derived from the Locations.csv file in /Data
+
+    Attributes:
+        location_id (int): An identification number for a location
+        location_name (str): The name of the location
+        location_address (str): The street address of the location
+    """
+    
     def __init__(self, location_id, location_name, location_address):
+        
+        """
+        Initializes a new location instance.
+
+        ARGS:
+            location_id: the identifier as an integer
+            location_name: the name of the location as a string
+            location_address: the street address of the location as a string
+        """
+
         self.location_id = location_id
         self.location_name = location_name
         self.location_address = location_address
     
     def __str__(self):
+        
+        """
+        Returns a formatted string with the location_id, location_name, and location_address.
+        """
+        
         return (f"ID: {self.location_id:3} | Name: {self.location_name:30} | Address: {self.location_address:30}")
